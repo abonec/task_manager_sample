b402f8050ce13778dbb603424c29cfbf57008cff
abonec <abonec@gmail.com>
2016-04-13 15:02:15 +0300

    add create and update actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,26 @@
 class TasksController < ApplicationController
   before_filter :login_required
-  before_filter :find_task, only: [:edit, :show]
+  before_filter :find_task, only: [:edit, :show, :update]
   def index
     @tasks = current_user.tasks
     render text: current_user.email
+  end
+
+  def create
+    @task = current_user.tasks.new task_params
+    if @task.save
+      redirect_to @task
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @task.update_attributes(task_params)
+      redirect_to @task
+    else
+      render :edit
+    end
   end
 
   def edit
@@ -18,5 +35,9 @@ class TasksController < ApplicationController
   def find_task
     @task = current_user.tasks.where(id: params[:id]).first
     redirect_to tasks_path unless @task
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :description)
   end
 end
--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 describe TasksController, type: :controller do
   describe 'should redirect if not signed in' do
-    {index: {method: :get}, edit: {method: :get, params: {id: 1}}}.each do |action, options|
+    {
+        index: {method: :get},
+        edit: {method: :get, params: {id: 1}},
+        show: {method: :get, params: {id: 1}},
+    }.each do |action, options|
       it "from #{action}" do
         send(options[:method], action, options[:params])
         expect(response).to redirect_to new_sessions_path
@@ -36,6 +40,39 @@ describe TasksController, type: :controller do
           expect(@task.user).not_to eq(@user)
           expect(response).to redirect_to tasks_path
         end
+      end
+
+      let (:task_attributes) {
+        { name: 'task_name', description: 'description', }
+      }
+      it 'and create task' do
+        expect {
+          post :create, task: task_attributes
+        }.to change(Task, :count).to 1
+
+        @task = assigns(:task)
+        expect(@task.name).to eq(task_attributes[:name])
+        expect(@task.description).to eq(task_attributes[:description])
+        expect(@task).to be_persisted
+        expect(@task.user).to eq(@user)
+
+        expect(response).to redirect_to(task_path(@task))
+      end
+
+      it 'and update task' do
+        @task = create(:task, user: @user)
+        expect {
+          put :update, id: @task.id, task: task_attributes
+        }.not_to change(Task, :count)
+
+        @edited_task = assigns(:task)
+        expect(@task.id).to eq(@edited_task.id)
+        task_attributes.each do |attribute, value|
+          expect(@edited_task[attribute]).not_to eq(@task[attribute])
+          expect(@edited_task[attribute]).to eq(value)
+        end
+
+        expect(response).to redirect_to(task_path(@edited_task))
       end
     end
   end
