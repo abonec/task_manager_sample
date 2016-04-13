f7c5e72e2770446d4de998c549f00d39f96ca117
abonec <abonec@gmail.com>
2016-04-13 13:44:39 +0300

    add edit and show actions for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,22 @@
 class TasksController < ApplicationController
   before_filter :login_required
+  before_filter :find_task, only: [:edit, :show]
   def index
     @tasks = current_user.tasks
     render text: current_user.email
+  end
+
+  def edit
+    render text: @task.name
+  end
+
+  def show
+    render text: @task.name
+  end
+
+  private
+  def find_task
+    @task = current_user.tasks.where(id: params[:id]).first
+    redirect_to tasks_path unless @task
   end
 end
--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -2,20 +2,42 @@ require 'rails_helper'
 
 describe TasksController, type: :controller do
   describe 'should redirect if not signed in' do
-    %i(index).each do |action|
+    {index: {method: :get}, edit: {method: :get, params: {id: 1}}}.each do |action, options|
       it "from #{action}" do
-        get action
+        send(options[:method], action, options[:params])
         expect(response).to redirect_to new_sessions_path
       end
     end
   end
-  describe 'should respond with tasks' do
-    it 'if it is just user' do
-      @user = create(:user)
-      @task = create(:task, user: @user)
-      sign_in(@user)
-      get :index
-      expect(assigns(:tasks)).to eq([@task])
+  describe 'should works with tasks' do
+    describe 'as normal user' do
+      before :each do
+        @user = create(:user)
+        sign_in @user
+      end
+
+      it 'and show all own tasks' do
+        @task1 = create(:task, user: @user)
+        @task2 = create(:task, user: @user)
+        get :index
+        expect(assigns(:tasks).sort).to eq([@task1, @task2].sort)
+      end
+
+      %i(show edit).each do |action|
+        it "and #{action} task" do
+          @task = create(:task, user: @user)
+          get action, id: @task.id
+          expect(assigns(:task)).to eq(@task)
+        end
+
+        it "and redirect to tasks if user don't have permission for #{action} task" do
+          @task = create(:task)
+          get action, id: @task.id
+          expect(@task.user).not_to eq(@user)
+          expect(response).to redirect_to tasks_path
+        end
+      end
     end
   end
+
 end
